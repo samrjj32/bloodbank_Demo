45eaded8c28a104d8afd7309ad9cec9c8ad433ba
samrajj32 <samrajj3223@gmail.com>
2025-03-16 16:39:58 +0000

    auth console

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -76,6 +76,7 @@ router.post('/register', async (req, res) => {
 
 // Login
 router.post('/login', async (req, res) => {
+  console.log(req,res,"loginxxx");
   try {
     const { email, password } = req.body;
 
